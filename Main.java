5dd19896918656c0fe433117ef6e09fef7148114
Parjad Minooei <155120660+ParjadM@users.noreply.github.com>
2024-09-28 22:29:06 -0400

    Add files via upload
    Every Developer needs a hello world specially in java!

---
+++ Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //This Hello World in java marks my professional development
+        //In java learning data structure and more
+        System.out.println ("HELLO WORLD!!!");
+    }
+}
